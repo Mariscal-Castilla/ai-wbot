1ac9293e49da9f7a8b54d02b31fcdf3b2d79da22
Alexito-Hub <alessandrovillogas@outlook.es>
2024-11-25 21:21:15 -0500

    first commit

--- index.js
+++ index.js
@@ -199,7 +199,7 @@ const start = async() => {
 			}
 
 			case "sotito": {
-				let { data: prmpt } = await axios.get("https://raw.githubusercontent.com/al-e-dev/prompt/refs/heads/main/ai-wbot.txt")
+				let { data: prmpt } = await axios.get("https://raw.githubusercontent.com/al-e-dev/prompt/refs/heads/main/aiwbot.txt")
                     
 				if (type === "audioMessage") return client.sendMessage(from, { text: "Aun no podemos escuchar tu voz, vuelve cuando recibas una actualizacion." })
 				if (type === "videoMessage" || type === "imageMessage" ) return client.sendMessage(from, { text: "Aun no podemos ver las imagenes vuelve cuando recibas una actualizacion." })
@@ -269,12 +269,9 @@ const start = async() => {
 			}
 			default: {
 			    // DEFAULT INTELIGENCE
-			    if (body && type === "imageMessage" || type === "videoMessage" || type === "audioMessage" && !from.endsWith("@g.us") && !v.key.fromMe) {
-                    let { data: prmpt } = await axios.get("https://raw.githubusercontent.com/al-e-dev/prompt/refs/heads/main/ai-wbot.txt")
-                     
-					if (type === "audioMessage") return client.sendMessage(from, { text: "Aun no podemos escuchar tu voz, vuelve cuando recibas una actualizacion." })
-					if (type === "videoMessage" || type == "imageMessage") return client.sendMessage(from, { text: "Aun no podemos ver las imagenes vuelve cuando recibas una actualizacion." })
-						
+			    if (body && !from.endsWith("@g.us") && !v.key.fromMe) {
+                    let { data: prmpt } = await axios.get("https://raw.githubusercontent.com/al-e-dev/prompt/refs/heads/main/aiwbot.txt")
+					
                     let hist = await TEMPORARY_CONVERSATION.findOne({ user: Number })
                 
                     if (hist && new Date() > hist.expiry) {
